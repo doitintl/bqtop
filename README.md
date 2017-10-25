94da4e3c458a30d051251c0bc9a5b37e58d262de
Aviv Laufer <aviv.laufer@gmail.com>
2017-10-25 11:52:40 +0300

    Formating + Readme
    Format a bit index.js
    Start working on the READM.md

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
-firebase list|grep -i BQTop |awk '{print $4}'
+#BQTop
 
+This repository contains the BQTop utility for viewing running and finshed Big-Query jobs.
+
+
+
+
+
+
+
+###For later use
+`firebase list|grep -i BQTop |awk '{print $4}
 ggcloud components install beta
-
-
 gcloud beta logging sinks create my-sink1
 pubsub.googleapis.com/projects/$MyProj/topics/my-sync
 --log-filter="resource.type="bigquery_resource"
 "protoPayload.methodName="jobservice.jobcompleted"
-
 resource.type="bigquery_resource" 
 protoPayload.methodName="jobservice.insert"
-
 cd functions; npm install
-firebase deploy --project <projectId>
+firebase deploy --project <projectId>`
--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,18 +1,4 @@
-/**
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 'use strict';
 
 // [START import]
@@ -25,68 +11,62 @@ var refFinished = db.ref('/finished-jobs');
 // [END import]
 
 // [START RunningJobsPubSub]
-/**
- * Cloud Function to be triggered by Pub/Sub that logs a message using the data published to the
- * topic.
- */
-// [START trigger]
-exports.RunningJobsPubSub = functions.pubsub.topic('bqps-running-jobs').onPublish(event => {
+exports.RunningJobsPubSub = functions.pubsub.topic('bqtop-running-jobs').onPublish(event => {
 // [END trigger]
     const pubSubMessage = event.data;
 return refRunning.push(pubSubMessage.json);
 })
-;
 // [END RunningJobsPubSub]
 
 
-// [START RunningJobsPubSub]
-/**
- * Cloud Function to be triggered by Pub/Sub that logs a message using the data published to the
- * topic.
- */
-// [START trigger]
-exports.FinishedJobsPubSub = functions.pubsub.topic('bqps-finished-jobs').onPublish(event => {
-// [END trigger]
-   const pubSubMessage = event.data;
-   return refFinished.push(pubSubMessage.json);
+// [START FinishedJobsPubSub]
+exports.FinishedJobsPubSub = functions.pubsub.topic('bqtop-finished-jobs').onPublish(event = > {
+
+    const pubSubMessage = event.data;
+return refFinished.push(pubSubMessage.json);
 })
-;
 
+// [END FinishedJobsPubSub]
 
-// [END finishedJobsPubSub]
-exports.DeleteRunningJobFromFinished = functions.database.ref('finished-jobs/{key}').onCreate(event => {
-      const runningRef = event.data.adminRef.root.child('running-jobs')
-      const jobId = event.data.val()['protoPayload']['serviceData']['jobCompletedEvent']['job']['jobName']['jobId']
-      runningRef.orderByChild('protoPayload/serviceData/jobInsertResponse/resource/jobName/jobId')
-          .equalTo(jobId).once("value", function (snapshot) {
-              snapshot.forEach(function (data) {
-          runningRef.child(data.key).remove()
-
-    })
-
-});
-
-});
-
-exports.DeleteRunningJobFromRunning = functions.database.ref('running-jobs/{key}').onCreate(event => {
-    const runningRef = event.data.adminRef.root.child('running-jobs')
-    const finishedRef = event.data.adminRef.root.child('finished-jobs')
-    const jobId = event.data.val()['protoPayload']['serviceData']['jobInsertResponse']['resource']['jobName']['jobId']
-    finishedRef.orderByChild('protoPayload/serviceData/jobCompletedEvent/job/jobName/jobId').equalTo(jobId).once("value", function (snapshot) {
-    snapshot.forEach(function (data) {
-        runningRef.orderByChild('protoPayload/serviceData/jobInsertResponse/resource/jobName/jobId').
-        equalTo(jobId)
-            .once("value", function (snapshot) {
-            snapshot.forEach(function (data) {
+// [START DeleteRunningJobFromFinished]
+exports.DeleteRunningJobFromFinished = functions.database.ref('finished-jobs/{key}').onCreate(event =>
+    {
+        const runningRef = event.data.adminRef.root.child('running-jobs')
+        const jobId = event.data.val()['protoPayload']['serviceData']['jobCompletedEvent']['job']['jobName']['jobId']
+        runningRef.orderByChild('protoPayload/serviceData/jobInsertResponse/resource/jobName/jobId')
+        .equalTo(jobId).once("value", function (snapshot) {
+        snapshot.forEach(function (data) {
             runningRef.child(data.key).remove()
 
-            })
-
-        });
-
-
     })
 
 });
 
+})
+// [END DeleteRunningJobFromFinished]
+
+
+// [START DeleteRunningJobFromRunning]
+exports.DeleteRunningJobFromRunning = functions.database.ref('running-jobs/{key}').onCreate(event => {
+    const runningRef = event.data.adminRef.root.child('running-jobs');
+    const finishedRef = event.data.adminRef.root.child('finished-jobs');
+    const jobId = event.data.val()['protoPayload']['serviceData']['jobInsertResponse']['resource']['jobName']['jobId']
+    finishedRef.orderByChild('protoPayload/serviceData/jobCompletedEvent/job/jobName/jobId').
+    equalTo(jobId).once("value", function (snapshot) {
+        snapshot.forEach(function (data) {
+            runningRef.orderByChild('protoPayload/serviceData/jobInsertResponse/resource/jobName/jobId').equalTo(jobId)
+                .once("value", function (snapshot) {
+                    snapshot.forEach(function (data) {
+                        runningRef.child(data.key).remove()
+
+                    })
+
+                });
+
+
+        })
+
 });
+
+})
+// [END DeleteRunningJobFromRunning]
