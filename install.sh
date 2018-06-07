4ceaeb7e1d338287fd384851f2b6e034c02b6f94
drorlevy <drorlevy@gmail.com>
2018-06-07 11:36:39 +0300

    - Update sink filter to ignore dry runs query jobs - Install packages before creating resources

--- install.sh
+++ install.sh
@@ -26,28 +26,38 @@ if [ $# -eq 0 ]
   then
     error_exit "No arguments supplied"
 fi
-PROJECTID=`firebase list | grep -i $1 | awk 'BEGIN { FS="│" } { printf $3 }' | sed 's/ //g'`
-if [ -z "$PROJECTID" ]; then
+PROJECTID=`firebase list | grep -i -w $1 | awk 'BEGIN { FS="│" } { printf $3 }' | sed 's/ //g'`
+if [ "$1" != "$PROJECTID" ]; then
  echo Project $1 Not Found!
  exit 
 fi
-echo Project ID $PROJECTID
+echo Project ID: $PROJECTID
 gcloud config set project $PROJECTID
+
 echo -n "* Installing NPM Packages..."
+
 npm install -g firebase-tools
 cd firebase/functions/
 npm install
-cd ../..
+cd ../ui
+if [ ! -f .env.production ]; then
+    echo "File .env.production not found in firebase/ui !"
+    exit
+fi
+yarn install
+yarn run build
+cd ..
+
 echo -n "* Creating Pub/Sub topics..."
 
 gcloud beta pubsub topics create bqtop-running-jobs --project=$PROJECTID --quiet >/dev/null || error_exit "Error creating Pub/Sub topics"
 gcloud beta pubsub topics create bqtop-finished-jobs --project=$PROJECTID --quiet >/dev/null || error_exit "Error creating Pub/Sub topics"
 
-echo "done"
+echo "Create topics: done."
 
 echo -n "* Creating Log Export sinks..."
 
-gcloud logging sinks create bqtop-running-jobs-export pubsub.googleapis.com/projects/$PROJECTID/topics/bqtop-running-jobs --project=$PROJECTID --log-filter 'resource.type="bigquery_resource" protoPayload.methodName="jobservice.insert" protoPayload.serviceData.jobInsertRequest.resource.jobConfiguration.query:*' --quiet >/dev/null || error_exit "Error creating Log Export sink"
+gcloud logging sinks create bqtop-running-jobs-export pubsub.googleapis.com/projects/$PROJECTID/topics/bqtop-running-jobs --project=$PROJECTID --log-filter 'resource.type="bigquery_resource" protoPayload.methodName="jobservice.insert" protoPayload.serviceData.jobInsertRequest.resource.jobConfiguration.dryRun!=true protoPayload.serviceData.jobInsertRequest.resource.jobConfiguration.query:*' --quiet >/dev/null || error_exit "Error creating Log Export sink"
 
 ServiceAccountR=`gcloud logging sinks describe bqtop-running-jobs-export|grep writerIdentity|awk '{print $2}'`
 gcloud projects add-iam-policy-binding $PROJECTID --member=$ServiceAccountR --role='roles/pubsub.publisher'  --quiet >/dev/null || error_exit "Error creating Log Export sink"
@@ -56,18 +66,12 @@ gcloud logging sinks create bqtop-finished-jobs-export pubsub.googleapis.com/pro
 
 ServiceAccountF=`gcloud logging sinks describe bqtop-finished-jobs-export|grep writerIdentity|awk '{print $2}'`
 gcloud projects add-iam-policy-binding $PROJECTID --member=$ServiceAccountF --role='roles/pubsub.publisher'  --quiet >/dev/null || error_exit "Error creating Pub/Sub topics"
+echo "Create sinks: done."
 
-echo "done"
+echo -n "* Deploying app..."
 
-cd firebase/ui
-if [ ! -f .env.production ]; then
-    echo "File .env.production not found in firebase/ui !"
-    exit
-fi
-yarn install
-yarn run build
-
+firebase deploy --project $PROJECTID
 cd ..
-firebase use --add $PROJECTID
-firebase deploy
-cd ..
+
+echo "Deploy app: done."
+
